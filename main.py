e6c34194a52ac6af8d5b97f77b988bb5d0075a35
Pawel Wojcik <wojcikpwl@gmail.com>
2017-01-17 22:54:36 +0000

    main program updated to execute about function

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ TINYG PLOTTER - Main screen
 
 import os
 from helpers.menu import menu
+from helpers.about import about_info
 
 def main():
     title = "TINYG PLOTTER"
     active_menu = "Main"
-    main_menu_list = ["C - Connect",
+    menu_list = ["C - Connect",
                       "H - Homing",
                       "L - Load Program",
                       "S - Status",
@@ -19,8 +20,23 @@ def main():
                       "Q - Quit"]
 
     os.system("clear")      # clear screen
-    print(menu(title, active_menu, main_menu_list))     # print menu
-    input(" Select: ")      # wait for user selection
+    print(menu(title, active_menu, menu_list))     # print menu
+
+    selection = input(" Select: ")      # wait for user selection
+    if (selection == "a" or selection == "A"):
+        active_menu = "About"
+        menu_list = ["M - Main",
+                     "Q - Quit"]
+        os.system("clear")
+        print(menu(title, active_menu, menu_list))
+        print(about_info())
+        print(15*"-")
+        input(" Select: ")
+    else:
+        os.system("clear")
+        print("Wrong entry")
+        print(15*"-")
+        input(" Select: ")
 
 if __name__ == '__main__':
     main()
