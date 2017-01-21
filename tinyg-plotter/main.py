f483676ca9e77da1b5132ee2b208168a93ed02aa
Pawel Wojcik <wojcikpwl@gmail.com>
2017-01-21 15:42:59 +0000

    Open connection and homing functionality added

--- tinyg-plotter/main.py
+++ tinyg-plotter/main.py
@@ -6,12 +6,14 @@ TINYG PLOTTER - Main screen
 """
 
 import os
+import serial
+from time import sleep
 
+title = "TINYG PLOTTER"
 
 # Program main menu
 def main_menu():
     menu_width = 32
-    title = "TINYG PLOTTER"
     active_menu = "Main"
     menu_description = "* ITSligo - L7 in Mechatronics *"
     menu_list = ["C - Connect",
@@ -19,17 +21,45 @@ def main_menu():
                  "Q - Quit"]
 
     os.system("clear")      # clear screen
-
     print(menu(menu_width,title, active_menu, menu_description, menu_list))     # print menu
-
     selection = input("Select: ").lower()      # wait for user selection
-
-    if selection == "a":
+    if selection == "c":
+        connect()
+    elif selection == "a":
         about()
-
     elif selection == "q":
         close()
+    else:
+        invalid_selection()
 
+
+# Connect to the board
+def connect():
+    menu_width = 25
+    active_menu = "Connected"
+    menu_description = "Sucesfully connected"
+    menu_list = ["H - Home TinyG Plotter",
+                 "M - Main menu",
+                 "Q - Quit"]
+    os.system("clear")
+
+    # connection details goes here
+    ser = serial.Serial(port="/dev/cu.usbserial-DN00WNBE",baudrate=115200, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=1, xonxoff=False, rtscts=True, write_timeout=None, dsrdtr=False, inter_byte_timeout=None)
+    ser.close()
+    os.system("clear")
+    print(menu(menu_width, title, active_menu, menu_description, menu_list))
+
+    selection = input("Select: ").lower()
+    if selection == "m":
+        main_menu()
+    elif selection == "q":
+        close()
+    elif selection == "h":
+        ser.open()
+        sleep(0.5)
+        ser.write(str.encode("g28.2x0y0\n"))
+        ser.readlines()
+        ser.close()
     else:
         invalid_selection()
 
@@ -37,7 +67,6 @@ def main_menu():
 # About information
 def about():
     menu_width = 26
-    title = "TINYG PLOTTER"
     active_menu = "About"
     menu_description = "Author: Pawel Wojcik\nEmail: wojcikpwl@gmail.com\nVersion: 0.1"
     menu_list = ["M - Main menu",
@@ -64,11 +93,11 @@ def menu(width, title, menu_name, menu_description, menu_content):
             + "\n" + menu_description + "\n" + width * "-" + "\n"
             + "\n".join(menu_content) + "\n" + width * "-")
 
+
 # Wrong selection message
 def invalid_selection():
     os.system("clear")
     menu_width = 27
-    title = "TINYG PLOTTER"
     active_menu = "Error"
     menu_description = "Wrong entry"
     menu_list = ["Press ENTER to try again..."]
@@ -84,7 +113,6 @@ def invalid_selection():
 # Closing message
 def close():
     menu_width = 33
-    title = "TINYG PLOTTER"
     active_menu = "Exit"
     menu_description = "Thank you for using TinyG Plotter"
     menu_list = ["Press ENTER to quit..."]
